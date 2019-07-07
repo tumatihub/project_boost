00b99bfdba4dd90138efb4771cee49e8916c032b
tumatihub <jgsoaresdias@gmail.com>
2019-07-07 10:37:59 -0300

    PB_60

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(0,1)][SerializeField] private float movementFactor;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startPosition + offset;
+    }
+}
