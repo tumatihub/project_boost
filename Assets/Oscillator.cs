743e356e40cb4dbc9f46bcdb6ae34ff6dbcb2f48
tumatihub <jgsoaresdias@gmail.com>
2019-07-07 11:00:03 -0300

    PB_61

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
-    [Range(0,1)][SerializeField] private float movementFactor;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    float movementFactor;
     Vector3 startPosition;
 
     // Start is called before the first frame update
@@ -18,6 +19,11 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + .5f;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startPosition + offset;
     }
